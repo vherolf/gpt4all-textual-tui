4db5f00f0c6e60577e7f347b02fa0be1a8dfd2cb
Vherolf <vherolf@proclos.com>
2024-06-07 12:31:06 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+
+from textual import on
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer, RichLog, Input
+from textual.binding import Binding
+
+
+class GPTConsole(App):
+    """a basic mqtt console example"""
+    TITLE = "GPT Console"
+    BINDINGS = [Binding(key="q", action="quit_gpt_console", description="Quit App"),
+                Binding(key="c", action="clear_gpt_console", description="Clear Console"),]
+
+    def compose(self) -> ComposeResult:
+        yield Header(name=self.TITLE, show_clock=False)
+        yield Input(placeholder=f"Send a message")
+        yield RichLog()
+        yield Footer()
+
+    def on_mount(self):
+        pass
+
+    @on(Input.Submitted)
+    async def input_submitted(self, message: Input.Submitted) -> None:
+        await self.ask_gpt(f"{message.value}")
+
+    async def ask_gpt(self, message):
+        # input gpt4all here
+        self.query_one(RichLog).write(f"{message}")
+    
+    def action_clear_gpt_console(self) -> None:
+        self.query_one(RichLog).clear()
+
+    def action_quit_gpt_console(self) -> None:
+        self.app.exit()
+
+if __name__ == "__main__":
+    app = GPTConsole()
+    app.run()
